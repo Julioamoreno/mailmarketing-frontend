0d8d7d9d9ad2fda5666782b6670e3e4d042e109f
Júlio Armando <julioarmando321@gmail.com>
2020-04-02 16:49:23 -0300

    :art: Adcionando estrutura react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Cadastrar from './components/CriarUsuario';
+import Inicial from './components/Inicio';
+
+
+import TokenProvider from '../src/context/token';
+import { ProtectedRoute, LogedRoute } from './components/auth/protect';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+      <BrowserRouter>
+      <TokenProvider>
+        <Header />
+        <Menu />
+        
+        <Switch>
+          <Route path='/' exact component={Login} />
+          <Route path='/cadastrar' component={Cadastrar} />
+          <ProtectedRoute path='/inicio' component={Inicial} />
+          <Route path='/*' component={()=> '404 Página não encontrada'} />
+        </Switch>
+
+        <Footer />
+        </TokenProvider>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'materialize-css/dist/css/materialize.min.css';
+require('materialize-css');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// if(!(localStorage.getItem("TOKEN_ID"))){ //teste a ausencia de token
+    ReactDOM.render(<App />, document.getElementById('root'));
+
 serviceWorker.unregister();
