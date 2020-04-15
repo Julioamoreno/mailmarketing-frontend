76774567617e0aa9b0b677f07a4b8613b92d0a28
Júlio Armando <julioarmando321@gmail.com>
2020-04-14 22:18:31 -0300

    :art: Criando novas rotas

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import Footer from './components/Footer';
 import Login from './components/Login';
 import Cadastrar from './components/CriarUsuario';
 import Inicial from './components/Inicio';
-
+import EmailList from './components/email/email-list';
+import Listas from './components/lists/lists-list';
+import emailNew from './components/email/email-new';
 
 import TokenProvider from '../src/context/token';
-import { ProtectedRoute, LogedRoute } from './components/auth/protect';
+import { ProtectedRoute } from './components/auth/protect';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
 class App extends Component{
-  constructor(props){
-    super(props);
-  }
+
   render() {
     return (
       <BrowserRouter>
@@ -29,6 +29,9 @@ class App extends Component{
           <Route path='/' exact component={Login} />
           <Route path='/cadastrar' component={Cadastrar} />
           <ProtectedRoute path='/inicio' component={Inicial} />
+          <ProtectedRoute exact path='/email' component={EmailList} />
+          <ProtectedRoute path='/listas' component={Listas} />
+          <ProtectedRoute path='/email/emailnew' component={emailNew} />
           <Route path='/*' component={()=> '404 Página não encontrada'} />
         </Switch>
 
