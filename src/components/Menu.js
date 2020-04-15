5a1fdcfdf3dc81829870c05ed063fbd40b0337cc
Júlio Armando <julioarmando321@gmail.com>
2020-04-14 22:25:14 -0300

    :lipstick: Add arquivo de estilo individual para o menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import '../App.css';
+import { useHistory } from 'react-router-dom';
+import './Menu.css';
 import { Logout } from './auth/protect';
 import { useToken } from '../context/token';
 
 function Menu(props){
     const { token, setToken } = useToken();
+    const history = useHistory();
+
+    function goInicio (e){
+        e.preventDefault();
+        history.push('/')
+    }
+    function goEmail (e){
+        e.preventDefault();
+        history.push('/email')
+    }
+    function goLista (e){
+        e.preventDefault();
+        history.push('/listas')
+    }
 
 
     if(!token){
@@ -24,9 +39,9 @@ function Menu(props){
                         <span>{localStorage.getItem("nome")}</span>
                     </div>
                 </li>
-                <li><a href="#"><i className="material-icons">home </i></a></li>
-                <li><a href="#"><i className="material-icons">email </i></a></li>
-                <li><a href="#"><i className="material-icons">supervisor_account </i></a></li>
+                <li><a href="#" onClick={goInicio}><i className="material-icons">home </i></a></li>
+                <li><a href="#" onClick={goEmail} ><i className="material-icons">email </i></a></li>
+                <li><a href="#" onClick={goLista} ><i className="material-icons">supervisor_account </i></a></li>
                 <li><a href="#"><i className="material-icons">lock </i></a></li>
                 <li><a href="#" onClick={() => { setToken(); Logout();}} ><i className="material-icons">exit_to_app </i></a></li>
             </ul>
