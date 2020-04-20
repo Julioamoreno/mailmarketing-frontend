fba6d83c9ac9e8e6c52f47a955fd08a742d7cbc3
Júlio Armando <julioarmando321@gmail.com>
2020-04-19 21:35:39 -0300

    :art: Adcionando form/funçao para criaçao de campanha

--- src/components/email/email-new.js
+++ src/components/email/email-new.js
@@ -1,21 +1,58 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import createCampaing from '../auth/email';
 import '../Conteudo.css';
 
-export default function emailNew() {
-  return (
+export default function EmailNew(props) {
+
+    const [titulo, setTitulo] = useState(0);
+    const [conteudo, setConteudo] = useState(0);
+    const [data, setData] = useState(0);
+    const [mensagem, setMensagem] = useState('');
+
+    function criarCampanha(e){
+        e.preventDefault()
+        if(!titulo || !conteudo || !data) {
+            setMensagem('Erro: Preencha todos os campos');
+        }else
+        createCampaing(titulo, conteudo, data, props);
+    }
+    return (
     <div className="conteudo">
         <div>
             <h5>Nova Campanha</h5>
         </div>
         <div className="row">
-            <div className="col s12 grey ligthen-1">
+            {mensagem}
+            <div className="col s12 gray">
                 <div className="card-content">
-                    <form action="">
+                    <form onSubmit={criarCampanha}>
                         <div className="input-field">
-                            <input type="text" id="campanhaTitle" v-model="data.title"/>
+                            <input
+                                type="text" 
+                                id="campanhaTitle" 
+                                onChange={e => {setTitulo(e.target.value)}}
+                            />
                             <label htmlFor="campanhaTitle">Titulo</label>
                         </div>
+                        <div className="input-field">
+                            <textarea 
+                                id="campanhaBody" 
+                                rows="10" 
+                                className="materialize-textarea"
+                                onChange={e => {setConteudo(e.target.value)}}
+                            >
+                            </textarea>
+                            <label htmlFor="campanhaBody">Conteudo</label>
+                        </div>
+                        <div className="input-field">
+                            <input 
+                                type="text" 
+                                id="campanhaStart"
+                                onChange={e => {setData(e.target.value)}}
+                            />
+                            <label htmlFor="campanhaStart">Data de Início</label>
+                        </div>
+                        <input type="submit" value="Salvar" className="btn"/>
                     </form>
                 </div>
             </div>
---
+++ src/components/lists/lists-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function lists() {
+  return (
+    <div>
+        <h1>Listas de Disparo</h1>
+    </div>
+  );
+}
