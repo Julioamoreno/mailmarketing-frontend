ecaa2a9bb61a606dc78abb34101a457ed02b5ac2
Júlio Armando <julioarmando321@gmail.com>
2020-04-22 01:31:44 -0300

    :sparkles: Add funçao para ler e imprimindo dados na tela

--- src/components/lists/lists-list.js
+++ src/components/lists/lists-list.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory }  from 'react-router-dom';
+import { listAllLists } from '../auth/lists';
 
-// import { Container } from './styles';
+export default function Lists() {
+  let history = useHistory();
+  const [lists, setLists] = useState([]);
+ 
+  useEffect(() => {
+    async function fetchData() {
 
-export default function lists() {
+      const retorno = await listAllLists();
+      setLists(retorno);
+    }
+    fetchData();
+  },[history]);
+
+  function verLista (id, e) {
+    e.preventDefault();
+    history.push(`/listas/view/${id}`);
+  }
+
   return (
-    <div>
-        <h1>Listas de Disparo</h1>
+    <div className="linha">
+      <div className="col s12">
+        <h5>Listas de Disparo</h5>
+      </div>
+      <div className="col s12">
+        <div className="card">
+          <div className="card-content">
+            <table>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>titulo</th>
+                  <th>quantidade</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+              {lists.map( (item, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{item.title}</td>
+                  <td>{item.quantity}</td>
+                  <td>
+                    <a href="#" onClick={(event)=>verLista(item._id, event)} className="btn">ver</a>
+                  </td>
+                </tr>
+              ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
