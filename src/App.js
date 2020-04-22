3286ddd41ea7a98323f5cef5ef50ca30c30bbb2c
Júlio Armando <julioarmando321@gmail.com>
2020-04-22 01:34:03 -0300

    :art: Add rota para listagem de leads

--- src/App.js
+++ src/App.js
@@ -7,20 +7,19 @@ import Login from './components/Login';
 import Cadastrar from './components/CriarUsuario';
 import Inicial from './components/Inicio';
 import EmailList from './components/email/email-list';
-import Listas from './components/lists/lists-list';
 import emailNew from './components/email/email-new';
 import EmailView from './components/email/email-view';
 import EmailEdit from './components/email/email-edit';
 import EmailRemove from './components/email/email-remove';
+import Listas from './components/lists/lists-list';
+import ListsView from './components/lists/lists-view';
 
 import TokenProvider from '../src/context/token';
 import { ProtectedRoute } from './components/auth/protect';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-
 class App extends Component{
-
   render() {
     return (
       <BrowserRouter>
@@ -33,11 +32,12 @@ class App extends Component{
           <Route path='/cadastrar' component={Cadastrar} />
           <ProtectedRoute path='/inicio' component={Inicial} />
           <ProtectedRoute exact path='/email' component={EmailList} />
-          <ProtectedRoute path='/listas' component={Listas} />
           <ProtectedRoute path='/email/emailnew' component={emailNew} />
           <ProtectedRoute path='/email/view/:id' component={EmailView} />
           <ProtectedRoute path='/email/edit/:id' component={EmailEdit} />
           <ProtectedRoute path='/email/remove/:id' component={EmailRemove} />
+          <ProtectedRoute exact path='/listas' component={Listas} />
+          <ProtectedRoute path='/listas/view/:id' component={ListsView} />
           <Route path='/*' component={()=> '404 Página não encontrada'} />
         </Switch>
 
